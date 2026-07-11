3509f3651b2de0ab242407af50d6321fc2d286a0
aadi-blip-lab <aadipnwr@gmail.com>
2026-07-12 03:09:47 +0530

    Add exposure, brightness, and saturation effects

--- js/editor.js
+++ js/editor.js
@@ -2,10 +2,14 @@ import "./uploader.js";
 
 import { registerEffect } from "./renderer.js";
 
+import exposure from "../effects/exposure.js";
+import brightness from "../effects/brightness.js";
 import contrast from "../effects/contrast.js";
+import saturation from "../effects/saturation.js";
 
+registerEffect(exposure);
 registerEffect(brightness);
 registerEffect(contrast);
-registerEffect(fuji);
+registerEffect(saturation);
 
 import "./ui/sidebar.js";
