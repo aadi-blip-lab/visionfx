b2f05e80d0a88de057cbb575176e9d73629e1a80
aadi-blip-lab <aadipnwr@gmail.com>
2026-07-12 03:09:22 +0530

    Create saturation.js

---
+++ js/  effects/saturation.js
@@ -0,0 +1,25 @@
+export default function saturation(imageData, effects) {
+
+    const amount = effects.saturation;
+
+    if (amount === 0) return;
+
+    const data = imageData.data;
+
+    const factor = 1 + amount / 100;
+
+    for (let i = 0; i < data.length; i += 4) {
+
+        const r = data[i];
+        const g = data[i + 1];
+        const b = data[i + 2];
+
+        const gray = 0.299 * r + 0.587 * g + 0.114 * b;
+
+        data[i]     = gray + (r - gray) * factor;
+        data[i + 1] = gray + (g - gray) * factor;
+        data[i + 2] = gray + (b - gray) * factor;
+
+    }
+
+}
