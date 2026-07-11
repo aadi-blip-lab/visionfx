4a834b40a2d050c38928447b88226f6a866b2336
aadi-blip-lab <aadipnwr@gmail.com>
2026-07-12 03:00:05 +0530

    Update editor.js

--- js/editor.js
+++ js/editor.js
@@ -1,1 +1,9 @@
 import "./uploader.js";
+
+import { registerEffect } from "./renderer.js";
+
+import contrast from "../effects/contrast.js";
+
+registerEffect(contrast);
+
+import "./ui/sidebar.js";
