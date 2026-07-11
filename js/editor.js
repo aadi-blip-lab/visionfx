d83264398a4f21cedb319647ed5736c4aefef444
aadi-blip-lab <aadipnwr@gmail.com>
2026-07-12 03:16:46 +0530

    Update import paths and register effects in editor.js

--- js/editor.js
+++ js/editor.js
@@ -1,15 +1,14 @@
 import "./uploader.js";
+import "./ui/sidebar.js";
 
 import { registerEffect } from "./renderer.js";
 
-import exposure from "../effects/exposure.js";
-import brightness from "../effects/brightness.js";
-import contrast from "../effects/contrast.js";
-import saturation from "../effects/saturation.js";
+import exposure from "./effects/exposure.js";
+import brightness from "./effects/brightness.js";
+import contrast from "./effects/contrast.js";
+import saturation from "./effects/saturation.js";
 
 registerEffect(exposure);
 registerEffect(brightness);
 registerEffect(contrast);
 registerEffect(saturation);
-
-import "./ui/sidebar.js";
