0ff9d34f6ba64d4333e16ee827410a84f386a9c0
aadi-blip-lab <aadipnwr@gmail.com>
2026-07-12 03:53:31 +0530

    Update editor.js

--- js/editor.js
+++ js/editor.js
@@ -1,14 +1,3 @@
 import "./uploader.js";
-import "./ui/sidebar.js";
 
-import { registerEffect } from "./renderer.js";
-
-import exposure from "./effects/exposure.js";
-import brightness from "./effects/brightness.js";
-import contrast from "./effects/contrast.js";
-import saturation from "./effects/saturation.js";
-
-registerEffect(exposure);
-registerEffect(brightness);
-registerEffect(contrast);
-registerEffect(saturation);
+console.log("Editor Loaded");
