edd258da7689adfdd94561561d5d1ae2c34b3035
aadi-blip-lab <aadipnwr@gmail.com>
2026-07-12 02:52:33 +0530

    Update state.js

--- js/state.js
+++ js/state.js
@@ -1,1 +1,57 @@
+export const state = {
 
+    source: null,
+
+    type: null,
+
+    canvas: null,
+
+    ctx: null,
+
+    effects:{
+
+        contrast:1,
+
+        exposure:0,
+
+        brightness:0,
+
+        saturation:1,
+
+        highlights:0,
+
+        shadows:0,
+
+        blacks:0,
+
+        whites:0,
+
+        grain:0,
+
+        bloom:0,
+
+        glow:0,
+
+        halation:0,
+
+        vignette:0,
+
+        sharpen:0,
+
+        blur:0,
+
+        temperature:0,
+
+        tint:0,
+
+        fuji:false,
+
+        kodak:false,
+
+        noir:false,
+
+        hud:false
+
+    }
+
+};
