4a9cde3f36646cbac533df84d5fd47e9d99d8096
aadi-blip-lab <aadipnwr@gmail.com>
2026-07-12 02:52:48 +0530

    Update renderer.js

--- js/renderer.js
+++ js/renderer.js
@@ -1,1 +1,18 @@
+import { state } from "./state.js";
 
+export function render(){
+
+    if(!state.source) return;
+
+    const canvas=state.canvas;
+
+    const ctx=state.ctx;
+
+    canvas.width=state.source.naturalWidth;
+    canvas.height=state.source.naturalHeight;
+
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+
+    ctx.drawImage(state.source,0,0);
+
+}
