342a4096086f21614187bf161faeede8086323cf
aadi-blip-lab <aadipnwr@gmail.com>
2026-07-12 03:06:11 +0530

    Update brightness.js

--- js/  effects/brightness.js
+++ js/  effects/brightness.js
@@ -1,1 +1,17 @@
+export default function brightness(imageData, effects) {
 
+    const amount = effects.brightness;
+
+    if (amount === 0) return;
+
+    const data = imageData.data;
+
+    for (let i = 0; i < data.length; i += 4) {
+
+        data[i] += amount;
+        data[i + 1] += amount;
+        data[i + 2] += amount;
+
+    }
+
+}
