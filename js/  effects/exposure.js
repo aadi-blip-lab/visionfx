7dcbf726147fc6e05c0dcba03e16efcf81927de6
aadi-blip-lab <aadipnwr@gmail.com>
2026-07-12 03:08:44 +0530

    Create exposure.js

---
+++ js/  effects/exposure.js
@@ -0,0 +1,19 @@
+export default function exposure(imageData, effects) {
+
+    const value = effects.exposure;
+
+    if (value === 0) return;
+
+    const data = imageData.data;
+
+    const factor = value * 2;
+
+    for (let i = 0; i < data.length; i += 4) {
+
+        data[i] += factor;
+        data[i + 1] += factor;
+        data[i + 2] += factor;
+
+    }
+
+}
